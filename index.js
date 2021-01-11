8a05cc0a57bd6c506e2397ae18a34e15b3db2f4a
tien anh <qualtahaza@gmail.com>
2021-01-11 22:14:22 +0700

    he

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let sanpham = prompt("nhap vao ten sp ");
 if (sanpham == "ga") {
-  console.log("10$");
+    console.log("10$");
 } else if (sanpham == "vit") {
-  console.log("5$");
+    console.log("5$");
 } else if (sanpham == "bo") {
-  console.log("15$");
+    console.log("15$");
 } else if (sanpham == "cho") {
-  console.log("10$");
+    console.log("10$");
 }
+// sua
